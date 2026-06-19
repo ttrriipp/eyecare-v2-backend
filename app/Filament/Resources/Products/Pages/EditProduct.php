8350edaeb6ba5e0b832c217cc6fca4c4b6a7279b
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-06-19 10:47:10 +0800

    Slice 3: Delete/Restore actions on product edit page header

--- app/Filament/Resources/Products/Pages/EditProduct.php
+++ app/Filament/Resources/Products/Pages/EditProduct.php
@@ -3,9 +3,21 @@
 namespace App\Filament\Resources\Products\Pages;
 
 use App\Filament\Resources\Products\ProductResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\RestoreAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditProduct extends EditRecord
 {
     protected static string $resource = ProductResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            RestoreAction::make(),
+            DeleteAction::make(),
+            ForceDeleteAction::make(),
+        ];
+    }
 }
