b491c6eb56891c892b6a39a1f92778b2102c6e46
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-06-22 12:13:54 +0800

    ListBillings: add status tabs (All, Issued, Partially Paid, Paid, Voided)

--- app/Filament/Resources/Billings/Pages/ListBillings.php
+++ app/Filament/Resources/Billings/Pages/ListBillings.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\Billings\Pages;
 use App\Filament\Resources\Billings\BillingResource;
 use App\Filament\Resources\Billings\Widgets\BillingStatsWidget;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListBillings extends ListRecords
 {
@@ -15,5 +17,22 @@ class ListBillings extends ListRecords
         return [
             BillingStatsWidget::class,
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $statuses = ['issued', 'partially_paid', 'paid', 'voided'];
+
+        $tabs = ['all' => Tab::make('All')];
+
+        foreach ($statuses as $status) {
+            $tabs[$status] = Tab::make(ucwords(str_replace('_', ' ', $status)))
+                ->modifyQueryUsing(fn (Builder $query) => $query->whereHas(
+                    'status',
+                    fn (Builder $q) => $q->where('name', $status)
+                ));
+        }
+
+        return $tabs;
     }
 }
