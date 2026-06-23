8123b0c6751271c100bcc7e58882406d933d5b6a
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-06-23 16:13:13 +0800

    ListPatients: add New Patient create button to header

--- app/Filament/Resources/Patients/Pages/ListPatients.php
+++ app/Filament/Resources/Patients/Pages/ListPatients.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\Patients\Pages;
 
 use App\Filament\Resources\Patients\PatientResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPatients extends ListRecords
 {
     protected static string $resource = PatientResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()->label('New Patient'),
+        ];
+    }
 }
