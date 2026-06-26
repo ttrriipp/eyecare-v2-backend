111fe71386fc60e648ac94a52de1b2d718e41c6b
freezy29 <markgarcia2904@gmail.com>
2026-06-26 18:25:37 +0800

    fix: move view toggle outside table card into normal document flow

--- app/Filament/Resources/Appointments/Pages/ListAppointments.php
+++ app/Filament/Resources/Appointments/Pages/ListAppointments.php
@@ -3,15 +3,9 @@
 namespace App\Filament\Resources\Appointments\Pages;
 
 use App\Filament\Resources\Appointments\AppointmentResource;
-use App\Filament\Resources\Appointments\Widgets\AppointmentCalendarWidget;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Schemas\Components\EmbeddedTable;
-use Filament\Schemas\Components\Livewire;
-use Filament\Schemas\Components\RenderHook;
 use Filament\Schemas\Components\Tabs\Tab;
-use Filament\Schemas\Schema;
-use Filament\View\PanelsRenderHook;
 use Illuminate\Database\Eloquent\Builder;
 
 class ListAppointments extends ListRecords
@@ -21,17 +15,6 @@ class ListAppointments extends ListRecords
     protected string $view = 'filament.resources.appointments.pages.list-appointments';
 
     public bool $showCalendar = false;
-
-    public function content(Schema $schema): Schema
-    {
-        return $schema->components([
-            $this->getTabsContentComponent()->hidden(fn (): bool => $this->showCalendar),
-            RenderHook::make(PanelsRenderHook::RESOURCE_PAGES_LIST_RECORDS_TABLE_BEFORE)->hidden(fn (): bool => $this->showCalendar),
-            EmbeddedTable::make()->hidden(fn (): bool => $this->showCalendar),
-            RenderHook::make(PanelsRenderHook::RESOURCE_PAGES_LIST_RECORDS_TABLE_AFTER)->hidden(fn (): bool => $this->showCalendar),
-            Livewire::make(AppointmentCalendarWidget::class)->hidden(fn (): bool => ! $this->showCalendar),
-        ]);
-    }
 
     protected function getHeaderActions(): array
     {
