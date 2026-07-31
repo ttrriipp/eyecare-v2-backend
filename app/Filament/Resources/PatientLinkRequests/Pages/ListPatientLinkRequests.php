9a561562398147426b8e697089a94e36bdb36706
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-07-31 22:35:26 +0800

    fix: remove create action from Link Requests list

--- app/Filament/Resources/PatientLinkRequests/Pages/ListPatientLinkRequests.php
+++ app/Filament/Resources/PatientLinkRequests/Pages/ListPatientLinkRequests.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\PatientLinkRequests\Pages;
 
 use App\Filament\Resources\PatientLinkRequests\PatientLinkRequestResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPatientLinkRequests extends ListRecords
 {
     protected static string $resource = PatientLinkRequestResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
 }
