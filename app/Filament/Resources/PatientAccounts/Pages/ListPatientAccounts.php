87c255f77f87448841eb33c7746d8b0385d00fbd
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-08-01 10:20:44 +0800

    fix: remove New Patient Account button from list page

--- app/Filament/Resources/PatientAccounts/Pages/ListPatientAccounts.php
+++ app/Filament/Resources/PatientAccounts/Pages/ListPatientAccounts.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\PatientAccounts\Pages;
 
 use App\Filament\Resources\PatientAccounts\PatientAccountResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPatientAccounts extends ListRecords
 {
     protected static string $resource = PatientAccountResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
 }
