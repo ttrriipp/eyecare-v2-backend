dbde5b3c3de96c1e7cb3f59ba61798d7b7fe2520
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-08-01 10:44:42 +0800

    fix: use Filament TableWidget for inline requests table
    AppointmentRequestsInlineTable now extends TableWidget which properly
    implements all required abstract methods from Filament's table system.

--- app/Livewire/AppointmentRequestsInlineTable.php
+++ app/Livewire/AppointmentRequestsInlineTable.php
@@ -4,17 +4,12 @@ namespace App\Livewire;
 
 use App\Filament\Resources\AppointmentRequests\Tables\AppointmentRequestsTable;
 use App\Models\AppointmentRequest;
-use Filament\Tables\Concerns\InteractsWithTable;
-use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
-use Illuminate\Contracts\View\View;
+use Filament\Widgets\TableWidget;
 use Illuminate\Database\Eloquent\Builder;
-use Livewire\Component;
 
-class AppointmentRequestsInlineTable extends Component implements HasTable
+class AppointmentRequestsInlineTable extends TableWidget
 {
-    use InteractsWithTable;
-
     protected function getTableQuery(): Builder
     {
         return AppointmentRequest::query()->orderBy('created_at', 'desc');
@@ -23,10 +18,5 @@ class AppointmentRequestsInlineTable extends Component implements HasTable
     public function table(Table $table): Table
     {
         return AppointmentRequestsTable::configure($table);
-    }
-
-    public function render(): View
-    {
-        return view('livewire.appointment-requests-inline-table');
     }
 }
