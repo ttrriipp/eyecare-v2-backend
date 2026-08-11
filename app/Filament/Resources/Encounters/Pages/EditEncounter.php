d4d73e58d3e8878503f3b5bc90f56b484e413470
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-08-12 00:04:29 +0800

    feat: add service charge modal starts with 1 service line

--- app/Filament/Resources/Encounters/Pages/EditEncounter.php
+++ app/Filament/Resources/Encounters/Pages/EditEncounter.php
@@ -649,7 +649,7 @@ class EditEncounter extends EditRecord
                                 ]),
                         ])
                         ->columns(2)
-                        ->defaultItems(0)
+                        ->defaultItems(1)
                         ->minItems(1)
                         ->addActionLabel('Add Service Line'),
 
