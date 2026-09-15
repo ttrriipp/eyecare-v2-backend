025b7bddfce7f3f350ee2a7017c3048caa5e98b2
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-09-15 17:27:02 +0800

    fix: remove duplicate Get import in VariantForm

--- app/Filament/Resources/Products/Schemas/VariantForm.php
+++ app/Filament/Resources/Products/Schemas/VariantForm.php
@@ -9,7 +9,6 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Utilities\Get;
-use Filament\Schemas\Components\Utilities\Get;
 
 final class VariantForm
 {
