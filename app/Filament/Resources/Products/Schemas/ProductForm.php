786c8a156c272c8647acc61f796531c05ca7fc6a
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2026-09-16 00:18:11 +0800

    fix: add missing KeyValue and Get imports in ProductForm

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Products\Schemas;
 
 use App\Models\Product;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\KeyValue;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
@@ -11,6 +12,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Str;
